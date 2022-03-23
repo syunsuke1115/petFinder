11b05a136af7f4493f60ba1afe84a932d7e6890a
syunsuke1115 <kinopio1115@gmail.com>
2022-03-23 02:44:29 +0000

    transformerでも動くように

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ warnings.filterwarnings('ignore')
 torch.backends.cudnn.benchmark = True
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+class CFG: #モデルを変えるときはここを変更する
+    size=224 
+    model_name='tf_efficientnet_b0_ns' 
+    seed=42
+    model_path = './tf_efficientnet_b0_ns_fold0_best.pth'
+    isTransformer =False
+    
 class CnnModel(nn.Module):
     def __init__(self, cfg, pretrained=True):
         super().__init__()
@@ -31,19 +38,23 @@ class CnnModel(nn.Module):
         output = self.fc(feature)
         return output
 
-class CFG:
-    apex=False
-    debug=False
-    print_freq=10
-    num_workers=4
-    size=224 ##モデルによって変える。
-    model_name='tf_efficientnet_b0_ns' ##モデルによって変える
-    batch_size=16
-    seed=42
-    target_size=1
-    target_col='Pawpularity'
+class TransformerModel(nn.Module):
+    def __init__(self, cfg, pretrained=True):
+        super().__init__()
+        self.cfg = cfg
+        self.model   = timm.create_model(self.cfg.model_name, pretrained=pretrained, num_classes=0, in_chans=3)
+        num_features = self.model.num_features
+        self.linear  = nn.Linear(num_features, 1)
+        
+    def feature(self, image):
+        feature = self.model(image)
+        return feature
+    
+    def forward(self, x):
+        x = self.model(x)
+        output = self.linear(x)
+        return output
 
-#seed値を固定
 def set_seed(seed =42):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
@@ -51,7 +62,6 @@ def set_seed(seed =42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic =True
-set_seed(seed=CFG.seed) 
 
 def set_image(image):
     transform = transforms.Compose([
@@ -67,14 +77,29 @@ def set_image(image):
     img_batch = image[None]
     return img_batch
 
+def set_model(CFG):
+    if (CFG.isTransformer):
+        model = TransformerModel(CFG, pretrained=False)
+    else:
+        model = CnnModel(CFG, pretrained=False)
+    state = torch.load(CFG.model_path, 
+                        map_location=torch.device('cpu'))['model']
+    model.load_state_dict(state)
+    model.eval()
+    return model
+
 def get_score(result):
+    #testデータより算出。
+    #デプロイにデータベースを載せたくなかったので、別ファイルで計算した値をベタ打ち
     pawpularity_mean = 38.03904358353511
     pawpularity_std = 20.591990105774546
     deviation =((result-pawpularity_mean)/pawpularity_std*10 +50).round()
     result = result.round()
     return result, deviation
-    
+
+#インターフェース
 def main():
+    set_seed(seed=CFG.seed) 
     st.header("あなたのペットの可愛さを採点します")
     st.text("※kaggleコンペティションpetfinderのデータを使用しています")
     uploaded_file=st.file_uploader("画像アップロード")
@@ -83,13 +108,11 @@ def main():
         if uploaded_file is None:
             st.error('画像を提出してください')
             return
+        
         image = Image.open(uploaded_file)
         img_batch = set_image(image)
-        model = CnnModel(CFG, pretrained=False)
-        state = torch.load('./tf_efficientnet_b0_ns_fold0_best.pth', 
-                        map_location=torch.device('cpu'))['model']
-        model.load_state_dict(state)
-        model.eval()
+        model = set_model(CFG)
+        
         result= model(img_batch)
         result, deviation = get_score(result[0][0])
         
